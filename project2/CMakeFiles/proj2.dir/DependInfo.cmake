7108d64e29011247de1b32846783fa0755fb6a59
brian leeson <brianeleeson@gmail.com>
2018-01-17 10:18:01 -0800

    added ignore

--- project2/CMakeFiles/proj2.dir/DependInfo.cmake
+++ project2/CMakeFiles/proj2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/brian/workspace/410/project2/proj2.cxx" "/home/brian/workspace/410/project2/CMakeFiles/proj2.dir/proj2.cxx.o"
+  "/home/brian/workspace/410_SciVis/project2/proj2.cxx" "/home/brian/workspace/410_SciVis/project2/CMakeFiles/proj2.dir/proj2.cxx.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- project2/cmake_install.cmake
+++ project2/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/brian/workspace/410/project2
+# Install script for directory: /home/brian/workspace/410_SciVis/project2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/brian/workspace/410/project2/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/brian/workspace/410_SciVis/project2/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")

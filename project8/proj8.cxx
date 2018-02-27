35d560d73184accf1b8df7bc7a1af5b8c55a8ab3
brian leeson <brianeleeson@gmail.com>
2018-02-27 14:35:12 -0800

    starting proj8. skelton should do slide example

---
+++ project8/proj8.cxx
@@ -0,0 +1,76 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    SpecularSpheres.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+Credit: ---
+
+=========================================================================*/
+
+#include "vtkSmartPointer.h"
+#include "vtkSphereSource.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkActor.h"
+#include "vtkInteractorStyle.h"
+#include "vtkObjectFactory.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkProperty.h"
+#include "vtkCamera.h"
+#include "vtkLight.h"
+#include "vtkOpenGLPolyDataMapper.h"
+#include "vtkJPEGReader.h"
+#include "vtkImageData.h"
+#include <vtkPNGWriter.h>
+
+#include <vtkPolyData.h>
+#include <vtkPointData.h>
+#include <vtkPolyDataReader.h>
+#include <vtkPoints.h>
+#include <vtkUnsignedCharArray.h>
+#include <vtkFloatArray.h>
+#include <vtkDoubleArray.h>
+#include <vtkCellArray.h>
+#include <vtkDataSetReader.h>
+#include <vtkContourFilter.h>
+#include <vtkRectilinearGrid.h>
+
+#include <vtkCamera.h>
+#include <vtkDataSetMapper.h>
+#include <vtkRenderer.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkSmartPointer.h>
+
+
+
+
+int main()
+{	
+	//read data from file
+	vtkDataSetReader *rdr = vtkDataSetReader::New();
+    rdr->SetFileName("proj8.vtk");
+
+	// Apply filters
+	wtkContourFulter *cf = vtkContourFilter::New();
+	cf->SetNumberOfContours(1);
+	cf->SetValue(0,3.0);
+	cf->SetInputConnection(rdr->GetOutputPort());
+
+	// Write to File
+	vtkDataSetWriter *wrtr = vtkDataSetWriter::New();
+	wrtr->SetFileName("contour.vtk");
+	wrtr->SetInputConnection(cf->GetoutputPort());
+	wrtr->Write();
+	
+}

3c6e57ede8c60688cce8a61d7e36791f6050ee8f
brian leeson <brianeleeson@gmail.com>
2018-02-27 19:35:41 -0800

    compiles, runs. spheres in each corner. saving

--- project8/proj8.cxx
+++ project8/proj8.cxx
@@ -54,28 +54,113 @@ Credit: ---
 
 #include <vtkDataSetWriter.h>
 
-
-
 int main()
 {	
+	
 	//read data from file
 	vtkDataSetReader *rdr = vtkDataSetReader::New();
     rdr->SetFileName("proj8.vtk");
 
+	//TODO read data create some sort of filter/cutter/isosurface to pass to an actor thing
+
+	//test -- https://www.vtk.org/Wiki/VTK/Examples/Cxx/Filtering/ContoursFromPolyData
+	vtkSmartPointer<vtkPolyData> inputPolyData;
+	vtkSmartPointer<vtkSphereSource> sphereSource =
+      vtkSmartPointer<vtkSphereSource>::New();
+    sphereSource->SetThetaResolution(30);
+    sphereSource->SetPhiResolution(15);
+    sphereSource->Update();
+    inputPolyData = sphereSource->GetOutput();
+
+	vtkSmartPointer<vtkPolyDataMapper> inputMapper =
+		vtkSmartPointer<vtkPolyDataMapper>::New();
+
+	inputMapper->SetInputData(inputPolyData);
+
+	  // Create input actor
+	vtkSmartPointer<vtkActor> testActor = 
+		vtkSmartPointer<vtkActor>::New();
+	testActor->GetProperty()->SetColor(1.0, 0.8941, 0.7686); // bisque
+	testActor->SetMapper(inputMapper);
+ 
+
+	// Define viewport ranges
+	// (xmin, ymin, xmax, ymax)
+	double viewport1[4] = {0.0, 0.0, 0.5, 0.5};
+	double viewport2[4] = {0.0, 0.5, 0.5, 1.0};
+	double viewport3[4] = {0.5, 0.0, 1.0, 0.5};
+	double viewport4[4] = {0.5, 0.5, 1.0, 1.0};
+
+	// make renderers
+	vtkSmartPointer<vtkRenderer> ren1 = vtkSmartPointer<vtkRenderer>::New();
+	vtkSmartPointer<vtkRenderer> ren2 = vtkSmartPointer<vtkRenderer>::New();
+	vtkSmartPointer<vtkRenderer> ren3 = vtkSmartPointer<vtkRenderer>::New();
+	vtkSmartPointer<vtkRenderer> ren4 = vtkSmartPointer<vtkRenderer>::New();
+
+	ren1->SetViewport(viewport1);
+	ren2->SetViewport(viewport2);
+	ren3->SetViewport(viewport3);
+	ren4->SetViewport(viewport4);
+
+	ren1->AddActor(testActor); //display the sphere
+	ren2->AddActor(testActor); //display the sphere
+	ren3->AddActor(testActor); //display the sphere
+	ren4->AddActor(testActor); //display the sphere
+ 
+	//Add renderer to renderwindow and render
+	vtkSmartPointer<vtkRenderWindow> renderWindow =
+		vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(ren1);
+	renderWindow->AddRenderer(ren2);
+	renderWindow->AddRenderer(ren3);
+	renderWindow->AddRenderer(ren4);
+	renderWindow->SetSize(800, 800);
+
+
+	vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	interactor->SetRenderWindow(renderWindow);
+
+	
+	renderWindow->Render();
+
+	interactor->Start();
+	
+	return EXIT_SUCCESS;
+
+/*
 	// Apply filters
 	vtkContourFilter *cf = vtkContourFilter::New();
 	cf->SetNumberOfContours(1);
 	cf->SetValue(0,3.0);
 	cf->SetInputConnection(rdr->GetOutputPort());
 
-	// Write to File
-	vtkDataSetWriter *wrtr = vtkDataSetWriter::New();
-	wrtr->SetFileName("contour.vtk");
-	wrtr->SetInputConnection(cf->GetOutputPort());
-	wrtr->Write();
-
-	rdr->Delete();
-	cf->Delete();
-	wrtr->Delete();
 	
+	ren1->AddActor(testActor);
+
+
+	// make render window
+	vtkSmartPointer<vtkRenderWindow> renWin = 
+		vtkSmartPointer<vtkRenderWindow>::New();
+
+	// add renderers to render window
+	renWin->AddRenderer(ren1);
+	//renWin->AddRenderer(ren2);
+	//renWin->AddRenderer(ren3);
+	//renWin->AddRenderer(ren4);
+
+	renWin->SetSize(600, 600);
+
+	// make render window interactor - needed?
+	vtkSmartPointer<vtkRenderWindowInteractor> iren = 
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	iren->SetRenderWindow(renWin);
+
+
+	// this should show the picture
+	iren->Initialize();
+	iren->Start();
+
+	return 0;	
+	*/
 }
